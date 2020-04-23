c6a111dde2bbb5b6ae8e1b68335cdf160fe2eb2b
RichardTrombitas <richard.trombitas@yahoo.com>
2020-04-23 21:04:48 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NavBar from "./components/NavBar.js"
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js"
 import About from "./components/About.js"
-
 function App() {
   return (
     <>
