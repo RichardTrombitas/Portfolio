da974d59dedd021da841ad79fcf71b4bf21a0950
RichardTrombitas <richard.trombitas@yahoo.com>
2020-04-29 12:24:39 +0300

    Updates

---
+++ precache-manifest.f81d308487bcfa9379569ce84babb42f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc21e6dbce5e6f83b7c499f640e4734",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "202ba00b2b3d58ccb786",
+    "url": "/Portfolio/static/css/main.bc99a862.chunk.css"
+  },
+  {
+    "revision": "4b7b1d473f87671109a3",
+    "url": "/Portfolio/static/js/2.8990306c.chunk.js"
+  },
+  {
+    "revision": "639b92bbe3dcc3daca185fc2729a7bc5",
+    "url": "/Portfolio/static/js/2.8990306c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202ba00b2b3d58ccb786",
+    "url": "/Portfolio/static/js/main.bbaa03b4.chunk.js"
+  },
+  {
+    "revision": "107ce3c625a9fb1de279",
+    "url": "/Portfolio/static/js/runtime-main.e029fae7.js"
+  },
+  {
+    "revision": "05d642219d00ab5b5102a1e5afe2bfa6",
+    "url": "/Portfolio/static/media/Resume.05d64221.pdf"
+  },
+  {
+    "revision": "8e56357144d38dc251cacfaacae0dca2",
+    "url": "/Portfolio/static/media/calc.8e563571.png"
+  },
+  {
+    "revision": "a23e575e6a218b537b0b6687dd2c7e6a",
+    "url": "/Portfolio/static/media/logo.a23e575e.png"
+  },
+  {
+    "revision": "e1fc2715bc3ef985e9098bb9b2093944",
+    "url": "/Portfolio/static/media/msnLogo.e1fc2715.png"
+  },
+  {
+    "revision": "7c6e3f3b0b8ba5c33aa8a1848d908641",
+    "url": "/Portfolio/static/media/tictactoe.7c6e3f3b.png"
+  }
+]);
